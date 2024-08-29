0d547d321d2b6a25b37e28aea4aed4e47c505d20
Stefano Esposito <stefano.esposito@abstract.it>
2024-08-29 14:29:09 +0200

    mandukya component

---
+++ src/components/MandukyaUpanisad.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const MandukyaUpanisad = () => {
+    return (
+        <div>
+            <h2 className={'title'}>Mandukya Upanisad (testo)</h2>
+
+            <p>1. La sillaba Om è tutto l'universo. Eccone la spiegazione. Il passato, il presente, il futuro: tutto
+                ciò è [compreso nella] sillaba Om. E anche ciò che è al di là del tempo, che è triplice, è [compreso
+                nella] sillaba Om.</p>
+
+            <p>2. Infatti ogni cosa è il <i>Brahman</i>; l'<i>Atman</i> è il <i>Brahman</i>. Questo <i>Atman</i> ha
+                quattro modi di essere.</p>
+
+            <p>3. Il primo modo di essere si chiama vaisvanara ed è quando si ha lo stato di veglia, si ha la
+                conoscenza delle cose esteriori, sette membra, diciannove aperture e si godono gli elementi
+                materiali.</p>
+
+            <p>4. Il secondo modo di essere si chiama taijasa (luminoso) ed è quando si ha lo stato di sogno, si ha
+                la conoscenza delle cose interiori, sette membra, diciannove aperture e si godono gli elementi
+                sottili.</p>
+
+            <p>5. Quando l'uomo addormentato non concepisce alcun desiderio, non scorge alcun sogno, allora si ha
+                [lo stato di] sonno profondo. Il terzo modo di essere si chiama prajna ed è quando si ha lo stato di
+                sonno profondo, s'è raggiunta l'unità, si è costituiti soltanto di conoscenza, soltanto di gioia, si
+                gode la gioia, si ha per apertura (o strumento di percezione) il pensiero.</p>
+
+            <p>6. [Quando si trova in questa condizione, l'<i>Atman</i>] è il signore di tutto, è l'onnisciente, è
+                il reggitore interno, è il principio di tutte le cose, poiché è l'origine e la fine delle creature.
+            </p>
+
+            <p>7. Si considera come quarto [modo di essere] quello che è privo di conoscenza delle cose interiori,
+                privo di conoscenza delle cose esteriori, privo della conoscenza di entrambe. Esso non è costituito
+                soltanto di conoscenza, non è conoscitore né non conoscitore. Esso è invisibile, inavvicinabile,
+                inafferrabile, indefinibile, impensabile, indescrivibile, ha come caratteristica essenziale di
+                dipendere soltanto da se stesso; in esso il mondo visibile si risolve, è serenità e benevolenza, è
+                assolutamente non duale. Esso è l'<i>Atman</i>: esso deve essere conosciuto.</p>
+
+            <p>8. Per quel che riguarda i fonemi, questo <i>Atman</i> corrisponde alla sillaba Om, considerandone
+                gli elementi costitutivi. Gli elementi costitutivi corrispondono ai modi di essere, e i modi di
+                essere corrispondono agli elementi costitutivi, ossia ai suoni <strong>AUM</strong>.</p>
+
+            <p>9. Lo stato di veglia, <i>vaisvanara</i>, corrisponde alla lettera <strong>A</strong>, che è il primo
+                elemento, per il fatto che ottiene (ap) [tutto], oppure per il fatto che è il primo (adi). In verità
+                ottiene tutti i desideri e diventa il primo colui che così conosce.</p>
+
+            <p>10. Lo stato di sogno, <i>taijasa</i>, corrisponde alla lettera <strong>U</strong>, che è il secondo
+                elemento, per il fatto d'essere più alto (utkarsa) [del precedente] o per il fatto di partecipare
+                (ubhayatva) degli altri due [stati fra i quali si trova]. In verità colui che così conosce tiene
+                alta la tradizione della conoscenza [nella sua famiglia], è indifferente [a gioie e dolori] e nella
+                sua stirpe non nasce chi non conosca il <i>Brahman</i>.</p>
+
+            <p>11. Lo stato di sonno profondo, <i>prajna</i>, corrisponde alla lettera <strong>M</strong>, che è il
+                terzo elemento, per il fatto che crea (miti) o che [in esso] si dissolve (apiti) [l'universo]. In
+                verità colui che così conosce crea tutto questo universo e lo riassorbe in sé.</p>
+
+            <p>12. Il quarto [stato] non corrisponde a un [singolo] elemento, è inavvicinabile, in esso il mondo
+                visibile si risolve, è benevolenza, è assolutamente non duale.</p>
+            <hr className="elegant-hr" />
+        </div>
+    )
+}
+
+export default MandukyaUpanisad
